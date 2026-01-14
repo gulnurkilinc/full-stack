fbf4d7d8356519135f200b75893ef7e489ad97aa
Gülnur Kılınç <gulnurkilinc8@gmail.com>
2026-01-15 00:14:18 +0300

    backend ile frontend bağlandı.

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,12 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
+// Middleware - CORS'u düzgün ayarlayın
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cookieParser());
